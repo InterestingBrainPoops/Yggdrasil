ac1503555f898ddda7bd82c7015eb3dcf052178a
BrokenKeyboard <100025212@mvla.net>
2021-01-09 19:02:52 +0000

    added go abstractions, fixed somethings, and added a timer

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/InterestingBrainPoops/Yggdrasil
 
 go 1.14
 
-require github.com/BattlesnakeOfficial/rules v1.0.16 // indirect
+require github.com/BattlesnakeOfficial/rules v1.0.16
--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+  "github.com/BattlesnakeOfficial/rules"
+  "time"
 )
+var strl = rules.StandardRuleset{FoodSpawnChance:25, MinimumFood: 1}
 
 type Game struct {
 	ID      string `json:"id"`
@@ -15,8 +18,8 @@ type Game struct {
 }
 
 type Coord struct {
-	X int `json:"x"`
-	Y int `json:"y"`
+	X int32 `json:"x"`
+	Y int32 `json:"y"`
 }
 
 var UP = Coord{ X: 0, Y: 1}
@@ -35,8 +38,8 @@ type Battlesnake struct {
 }
 
 type Board struct {
-	Height int           `json:"height"`
-	Width  int           `json:"width"`
+	Height int32           `json:"height"`
+	Width  int32           `json:"width"`
 	Food   []Coord       `json:"food"`
 	Snakes []Battlesnake `json:"snakes"`
 }
@@ -51,7 +54,7 @@ type BattlesnakeInfoResponse struct {
 
 type GameRequest struct {
 	Game  Game        `json:"game"`
-	Turn  int         `json:"turn"`
+	Turn  int32         `json:"turn"`
 	Board Board       `json:"board"`
 	You   Battlesnake `json:"you"`
 }
@@ -64,6 +67,7 @@ type MoveResponse struct {
 type Move struct {
   Head Coord
   Direction string
+  ID string
 }
 
 // HandleIndex is called when your Battlesnake is created and refreshed
@@ -109,6 +113,103 @@ func (b Board) isInBounds(pos Coord) bool {
   return pos.X >= 0 && pos.X < b.Width && pos.Y >= 0 && pos.Y < b.Height
 }
 
+func newMove(direction string, vector Coord, snake Battlesnake) Move {
+  return Move {
+    Head: snake.Head.add(vector),
+    Direction: direction,
+    ID: snake.ID,
+  }
+}
+
+func (b Board) toBoardState() *rules.BoardState {
+  food := make([]rules.Point, len(b.Food))
+  for i, v := range b.Food {
+    food[i] = rules.Point(v)
+  }
+  
+  snakes := make([]rules.Snake, len(b.Snakes))
+  for i, v := range b.Snakes {
+    body := make([]rules.Point, len(v.Body))
+    for i, v := range v.Body {
+      body[i] = rules.Point(v)
+    }
+
+    snakes[i] = rules.Snake {
+      ID: v.ID,
+      Body: body,
+      Health: v.Health,
+    }
+  }
+  return &rules.BoardState{
+    Height: b.Height,
+    Width: b.Width,
+    Food: food,
+    Snakes: snakes,
+  }
+}
+
+func (b Board) getMoves() []rules.SnakeMove {
+  ret := make([]rules.SnakeMove, len(b.Snakes))
+  for i, snake := range b.Snakes {
+    possibleMoves := b.getValidMoves(snake) 
+    var move Move
+    if(len(possibleMoves ) == 0) {
+      // dying
+      move = Move{Direction:"up"};
+    } else {
+      move = possibleMoves[rand.Intn(len(possibleMoves))]
+    }
+    fmt.Printf("%s is moving %s\n", snake.Name, move.Direction)
+    ret[i] = rules.SnakeMove{
+      ID: snake.ID, 
+      Move: move.Direction,
+    }
+  }
+  return ret
+}
+
+func (b Board) getValidMoves(snake Battlesnake) []Move {
+  // Choose a random direction to move in
+	possibleMoves := []Move{
+    Move {
+     Direction: "up",
+     Head: snake.Head.add(UP),
+    }, 
+    Move {
+     Direction: "down",
+     Head: snake.Head.add(DOWN),
+    }, 
+    Move {
+     Direction: "left",
+     Head: snake.Head.add(LEFT),
+    }, 
+    Move {
+     Direction: "right",
+     Head: snake.Head.add(RIGHT),
+    },
+  }
+  // prune out the "invalid" moves. basically moves that will defenitely kill you
+
+  // hitting another snake
+  for _, v := range b.Snakes {
+    bodyMinusTail := v.Body[:len(v.Body)-1]
+    for _, segment := range bodyMinusTail {
+      possibleMoves = Filter(possibleMoves, 
+      func (move Move) bool {
+        return move.Head != segment 
+      })
+    }
+  }
+
+  // out of bounds
+  possibleMoves = Filter(possibleMoves, 
+      func (move Move) bool {
+        return b.isInBounds(move.Head)
+      })
+
+  return possibleMoves
+}
+
 // https://stackoverflow.com/questions/37334119/how-to-delete-an-element-from-a-slice-in-golang
 func remove(s []Move, i int) []Move {
   s[i] = s[len(s)-1]
@@ -133,54 +234,33 @@ func Filter(vs []Move, f func(Move) bool) []Move {
     return vsf
 }
 
+/*
+type SnakeMove struct {
+	ID   string
+	Move string
+}
+*/
+
+
 
 // HandleMove is called for each turn of each game.
 // Valid responses are "up", "down", "left", or "right".
 // TODO: Use the information in the GameRequest object to determine your next move.
 func HandleMove(w http.ResponseWriter, r *http.Request) {
+  start := time.Now()
 	request := GameRequest{}
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
 		log.Fatal(err)
 	}
   
-	// Choose a random direction to move in
-	possibleMoves := []Move{
-    Move {
-     Direction: "up",
-     Head: request.You.Head.add(UP),
-    }, 
-    Move {
-     Direction: "down",
-     Head: request.You.Head.add(DOWN),
-    }, 
-    Move {
-     Direction: "left",
-     Head: request.You.Head.add(LEFT),
-    }, 
-    Move {
-     Direction: "right",
-     Head: request.You.Head.add(RIGHT),
-    },
-  }
-  // prune out the "invalid" moves. basically moves that will defenitely kill you.
+  standardRules := rules.StandardRuleset{FoodSpawnChance : 25, MinimumFood : 1};
 
-  // hitting another snake
-  for _, v := range request.Board.Snakes { 
-    bodyMinusTail := v.Body[:len(v.Body)-1]
-    for _, segment := range bodyMinusTail {
-      possibleMoves = Filter(possibleMoves, 
-      func (move Move) bool {
-        return move.Head != segment 
-      })
-    }
-  }
+  boardState, err := standardRules.CreateNextBoardState(request.Board.toBoardState(), request.Board.getMoves())
+	
+  fmt.Printf("New boardstate: %v\n", boardState)
 
-  // out of bounds
-  possibleMoves = Filter(possibleMoves, 
-      func (move Move) bool {
-        return request.Board.isInBounds(move.Head)
-      })
+  possibleMoves := request.Board.getValidMoves(request.You)
   var move Move
   if(len(possibleMoves ) == 0) {
     fmt.Println("Dying now :(")
@@ -193,11 +273,14 @@ func HandleMove(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Printf("MOVE: %s\n", response.Direction)
+  endtime := time.Now()
+  fmt.Printf("TimeTaken: %d Microseconds\n", endtime.Sub(start).Microseconds());
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(response)
 	if err != nil {
 		log.Fatal(err)
 	}
+  
 }
 
 // HandleEnd is called when a game your Battlesnake was playing has ended.
