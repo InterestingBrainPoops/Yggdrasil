b457234b6d9b3ecdef33b7c94e0804a2683f5346
BrokenKeyboard <satasiyahet@gmail.com>
2021-02-12 10:33:03 -0800

    Update main.go
    update port to 8000 to reflect the ec2 open port

--- main.go
+++ main.go
@@ -234,7 +234,7 @@ func HandleEnd(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
-		port = "8080"
+		port = "8000"
 	}
 
 	http.HandleFunc("/", HandleIndex)
